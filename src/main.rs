36aef51b3fe446f5d688033dacd7143a0a09be78
Evan Almloff <evanalmloff@gmail.com>
2024-04-08 09:19:26 -0500

    fail to reproduce #2270

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+#![allow(non_snake_case)]
+
+use dioxus::prelude::*;
+use log::LevelFilter;
+
+#[derive(Clone, Routable, Debug, PartialEq)]
+enum Route {
+    #[route("/")]
+    Home {},
+    #[route("/blog/:id")]
+    Blog { id: i32 },
+}
+
+fn main() {
+    // Init debug
+    dioxus_logger::init(LevelFilter::Info).expect("failed to init logger");
+    console_error_panic_hook::set_once();
+
+    launch(App);
+}
+
+fn App() -> Element {
+    rsx! {
+        Router::<Route> {}
+    }
+}
+
+#[component]
+fn Blog(id: i32) -> Element {
+    rsx! {
+        Link { to: Route::Home {}, "Go to counter" }
+        "Blog post {id}"
+    }
+}
+
+#[component]
+fn Home() -> Element {
+    rsx! {
+        if true {}
+    }
+}
